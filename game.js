aeb1cb6b6d3381cce2e9a433aecbae972ba8b2a4
Sam Hearst <samhearst1013@gmail.com>
2020-12-04 18:58:41 -0500

    first steps of game

---
+++ game.js
@@ -0,0 +1,10 @@
+class Game {
+    constructor(playerOne, playerTwo) {
+        this.playerOneName = playerOne;
+        this.playerTwoName = playerTwo;
+    }
+
+    getName() {
+        return `${this.playerOneName} vs. ${this.playerTwoName}.`
+    }
+}
